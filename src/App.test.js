14141b5028016b32b8ad4efbcebf0937268ca34a
Grzegorz Zawiślak <zgrzegorz.gz@gmail.com>
2021-04-21 08:54:10 +0200

    Finish initializing project

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+/*import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+}); */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+it('renders without crashing', () => {
+  shallow(<App />);
 });
